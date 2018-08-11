0b415a55b38a880b1f83fe18be54daebaef82e98
Shinya Yamada <syamada@student.42.us.org>
2018-08-11 16:17:33 -0700

    add debug rule on make

---
+++ srcs/sort_helpers.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_helpers.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: syamada <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/11 16:02:48 by syamada           #+#    #+#             */
+/*   Updated: 2018/08/11 16:03:11 by syamada          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int		is_ascending(t_stack *stack)
+{
+	if (!stack)
+		return (0);
+	while (stack->next)
+	{
+		if (stack->data > stack->next->data)
+			return (0);
+		stack = stack->next;
+	}
+	return (1);
+}
+
+int		is_descending(t_stack *stack)
+{
+	if (!stack)
+		return (0);
+	while (stack->next)
+	{
+		if (stack->data < stack->next->data)
+			return (0);
+		stack = stack->next;
+	}
+	return (1);
+}
+
+void	swap_if_possible(t_stack **a, t_stack **b, t_oplist **oplist, char c)
+{
+	t_stack		*tmp;
+
+	tmp = c == 'a' ? *a : *b;
+	if (!tmp || !tmp->next)
+		return ;
+	if (tmp->data > tmp->next->data && c == 'a')
+		swap_a(a, b, oplist);
+	if (tmp->data < tmp->next->data && c == 'b')
+		swap_b(a, b, oplist);
+}
--- srcs/sort_stack.c
+++ srcs/sort_stack.c
@@ -6,50 +6,11 @@
 /*   By: syamada <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/11 08:36:25 by syamada           #+#    #+#             */
-/*   Updated: 2018/08/11 14:56:03 by syamada          ###   ########.fr       */
+/*   Updated: 2018/08/11 16:05:33 by syamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-int		is_ascending(t_stack *stack)
-{
-	if (!stack)
-		return (0);
-	while (stack->next)
-	{
-		if (stack->data > stack->next->data)
-			return (0);
-		stack = stack->next;
-	}
-	return (1);
-}
-
-int		is_descending(t_stack *stack)
-{
-	if (!stack)
-		return (0);
-	while (stack->next)
-	{
-		if (stack->data < stack->next->data)
-			return (0);
-		stack = stack->next;
-	}
-	return (1);
-}
-
-void	swap_if_possible(t_stack **a, t_stack **b, t_oplist **oplist, char c)
-{
-	t_stack		*tmp;
-
-	tmp = c == 'a' ? *a : *b;
-	if (!tmp || !tmp->next)
-		return ;
-	if (tmp->data > tmp->next->data && c == 'a')
-		swap_a(a, b, oplist);
-	if (tmp->data < tmp->next->data && c == 'b')
-		swap_b(a, b, oplist);
-}
 
 t_oplist	*sort(t_stack *a)
 {
