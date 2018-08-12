e28ed5228687933e8c16a550587ee18ece1aa68b
Shinya Yamada <syamada@student.42.us.org>
2018-08-11 17:36:46 -0700

    make insertion a little faster

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: syamada <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/07 15:43:21 by syamada           #+#    #+#             */
-/*   Updated: 2018/08/11 16:02:12 by syamada          ###   ########.fr       */
+/*   Updated: 2018/08/11 17:36:31 by syamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int		main(int argc, char **argv)
 	stack = NULL;
 	oplist = NULL;
 	stack = create_stack(stack, argv, option);
-	oplist = sort(stack);
+	oplist = insertion_sort(stack);
 	oplist = rev_oplist(oplist);
 	print_oplist(oplist);
 	return (0);
--- srcs/sort_stack.c
+++ srcs/sort_stack.c
@@ -6,13 +6,21 @@
 /*   By: syamada <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/11 08:36:25 by syamada           #+#    #+#             */
-/*   Updated: 2018/08/11 16:05:33 by syamada          ###   ########.fr       */
+/*   Updated: 2018/08/11 17:36:30 by syamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_oplist	*sort(t_stack *a)
+t_oplist	*quick_sort(t_stack *a)
+{
+	t_oplist	*oplist;
+
+	oplist = NULL;
+	return (oplist);
+}
+
+t_oplist	*insertion_sort(t_stack *a)
 {
 	t_stack 	*b;
 	t_oplist	*oplist;
@@ -23,9 +31,13 @@ t_oplist	*sort(t_stack *a)
 	if (is_ascending(a))
 		return (oplist);
 	swap_if_possible(&a, &b, &oplist, 'a');
+	if (is_ascending(a))
+		return (oplist);
 	push_b(&a, &b, &oplist);
 	while ((!is_ascending(a) || !is_descending(b)) || (a->data < b->data))
 	{
+		if (a->data > a->next->data)
+			swap_a(&a, &b, &oplist);
 		if (a->data > b->data)
 			push_b(&a, &b, &oplist);
 		else if (a->data < b->data)
