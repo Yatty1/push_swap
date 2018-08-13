940b083dd26db27ac1f92572dbfb5fe5b5f7127f
Shinya Yamada <syamada@student.42.us.org>
2018-08-12 21:32:50 -0700

    remove leak checker

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: syamada <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/07 15:43:21 by syamada           #+#    #+#             */
-/*   Updated: 2018/08/12 21:29:21 by syamada          ###   ########.fr       */
+/*   Updated: 2018/08/12 21:31:27 by syamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,5 @@ int		main(int argc, char **argv)
 	oplist = rev_oplist(oplist);
 	print_oplist(oplist);
 	oplistdel(&oplist);
-	while (1);
 	return (0);
 }
