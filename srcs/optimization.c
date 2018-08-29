1dae9b3919def05de6e65ac75cb808554659af3b
Shinya Yamada <syamada@student.42.us.org>
2018-08-28 18:45:04 -0700

    tune threshold

--- srcs/optimization.c
+++ srcs/optimization.c
@@ -6,7 +6,7 @@
 /*   By: syamada <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/12 16:29:13 by syamada           #+#    #+#             */
-/*   Updated: 2018/08/28 18:19:50 by syamada          ###   ########.fr       */
+/*   Updated: 2018/08/28 18:42:42 by syamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ t_oplist	*pick_algo(t_stack *stack)
 	len = stack_len(stack);
 	if (is_ascending(stack))
 		return (op);
-	if (len < 50)
+	if (len < 30)
 		op = small_sort(stack);
 	else
 		op = divide_sort(stack);
